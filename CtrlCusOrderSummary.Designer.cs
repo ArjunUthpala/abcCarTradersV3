c0100dfaa461f72686618ec46ba0566ed47beb54
Arjun Waththegedara <uthpala.arjun.8367@gmail.com>
2022-02-05 17:04:22 +0530

    order-type-filter-done

--- CtrlCusOrderSummary.Designer.cs
+++ CtrlCusOrderSummary.Designer.cs
@@ -35,29 +35,39 @@
             this.label10 = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtUsername = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewMyOrders = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBoxOrderType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMyOrders)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label14);
-            this.groupBox2.Location = new System.Drawing.Point(543, 524);
+            this.groupBox2.Location = new System.Drawing.Point(292, 546);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox2.Size = new System.Drawing.Size(418, 85);
+            this.groupBox2.Size = new System.Drawing.Size(418, 61);
             this.groupBox2.TabIndex = 77;
             this.groupBox2.TabStop = false;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(250, 33);
+            this.textBox1.Location = new System.Drawing.Point(250, 21);
             this.textBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(136, 27);
@@ -69,7 +79,7 @@
             this.label14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
             this.label14.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label14.Location = new System.Drawing.Point(30, 36);
+            this.label14.Location = new System.Drawing.Point(30, 24);
             this.label14.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(192, 22);
@@ -81,11 +91,11 @@
             this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.txtUsername);
-            this.groupBox1.Location = new System.Drawing.Point(15, 524);
+            this.groupBox1.Location = new System.Drawing.Point(15, 50);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(506, 85);
+            this.groupBox1.Size = new System.Drawing.Size(386, 61);
             this.groupBox1.TabIndex = 76;
             this.groupBox1.TabStop = false;
             // 
@@ -95,7 +105,7 @@
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label10.Location = new System.Drawing.Point(42, 37);
+            this.label10.Location = new System.Drawing.Point(16, 24);
             this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(88, 22);
@@ -106,7 +116,7 @@
             // 
             this.btnSearch.BackColor = System.Drawing.Color.Coral;
             this.btnSearch.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(330, 32);
+            this.btnSearch.Location = new System.Drawing.Point(232, 19);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(115, 30);
@@ -117,20 +127,20 @@
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(159, 34);
+            this.txtUsername.Location = new System.Drawing.Point(116, 21);
             this.txtUsername.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(136, 27);
+            this.txtUsername.Size = new System.Drawing.Size(95, 27);
             this.txtUsername.TabIndex = 67;
             // 
-            // dataGridView1
+            // dataGridViewMyOrders
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 65);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(946, 449);
-            this.dataGridView1.TabIndex = 75;
+            this.dataGridViewMyOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMyOrders.Location = new System.Drawing.Point(15, 132);
+            this.dataGridViewMyOrders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewMyOrders.Name = "dataGridViewMyOrders";
+            this.dataGridViewMyOrders.Size = new System.Drawing.Size(946, 404);
+            this.dataGridViewMyOrders.TabIndex = 75;
             // 
             // label1
             // 
@@ -145,24 +155,129 @@
             this.label1.TabIndex = 74;
             this.label1.Text = "Your Orders";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.comboBoxOrderType);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(414, 50);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(547, 61);
+            this.groupBox3.TabIndex = 78;
+            this.groupBox3.TabStop = false;
+            // 
+            // comboBoxOrderType
+            // 
+            this.comboBoxOrderType.FormattingEnabled = true;
+            this.comboBoxOrderType.Items.AddRange(new object[] {
+            "Car",
+            "Car Parts"});
+            this.comboBoxOrderType.Location = new System.Drawing.Point(237, 20);
+            this.comboBoxOrderType.Name = "comboBoxOrderType";
+            this.comboBoxOrderType.Size = new System.Drawing.Size(140, 28);
+            this.comboBoxOrderType.TabIndex = 70;
+            this.comboBoxOrderType.SelectedValueChanged += new System.EventHandler(this.comboBoxOrderType_SelectedValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(28, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 22);
+            this.label2.TabIndex = 69;
+            this.label2.Text = "Filter by Order Type";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(398, 19);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 69;
+            this.button1.Text = "Reset Filter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Location = new System.Drawing.Point(15, 50);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox4.Size = new System.Drawing.Size(386, 61);
+            this.groupBox4.TabIndex = 76;
+            this.groupBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(23, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 22);
+            this.label3.TabIndex = 66;
+            this.label3.Text = "Order ID";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(239, 19);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 30);
+            this.button2.TabIndex = 68;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(123, 21);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(95, 27);
+            this.textBox2.TabIndex = 67;
+            // 
             // CtrlCusOrderSummary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewMyOrders);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CtrlCusOrderSummary";
             this.Size = new System.Drawing.Size(977, 626);
+            this.Load += new System.EventHandler(this.CtrlCusOrderSummary_Load);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMyOrders)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,8 +291,16 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewMyOrders;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBoxOrderType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- CtrlCusOrderSummary.cs
+++ CtrlCusOrderSummary.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
+using DAL;
+using DAL.DAO;
+using DAL.DTO;
+using System.IO;
 
 namespace abcCarTradersV1
 {
     public partial class CtrlCusOrderSummary : UserControl
     {
+        string customer_NIC = Convert.ToString(UserStatic.NICNum);
         public CtrlCusOrderSummary()
         {
             InitializeComponent();
+        }
+
+        private void CtrlCusOrderSummary_Load(object sender, EventArgs e)
+        {
+           DisplayDateonLoad();
+        }
+
+        void DisplayDateonLoad()
+        {
+            List<tbl_cus_order> OrderDetails = new List<tbl_cus_order>();       
+            OrderDetails = CusOrderBLL.GetAllCusOrders(customer_NIC);    
+            dataGridViewMyOrders.DataSource = OrderDetails;
+            dataGridViewMyOrders.AutoGenerateColumns = false;
+            dataGridViewMyOrders.DefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Regular);
+            dataGridViewMyOrders.ColumnHeadersDefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Bold);
+            this.dataGridViewMyOrders.Columns["tbl_user"].Visible = false;
+        }
+
+   
+
+        private void comboBoxOrderType_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (comboBoxOrderType.SelectedIndex != -1)
+            {
+                if (comboBoxOrderType.SelectedItem.ToString() == "Car")
+                {
+                    string orderType = "Car";
+                    List<tbl_cus_order> OrderDetails = new List<tbl_cus_order>();
+                    OrderDetails = CusOrderBLL.GetOrderDetails(customer_NIC, orderType);
+                    dataGridViewMyOrders.DataSource = OrderDetails;
+                    dataGridViewMyOrders.AutoGenerateColumns = false;
+                    dataGridViewMyOrders.DefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Regular);
+                    dataGridViewMyOrders.ColumnHeadersDefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Bold);
+                    this.dataGridViewMyOrders.Columns["tbl_user"].Visible = false;
+                    this.dataGridViewMyOrders.Columns["OrderType"].Visible = false;
+                }
+                else
+                {
+                    string orderType = "Car Part";
+                    List<tbl_cus_order> OrderDetails = new List<tbl_cus_order>();
+                    OrderDetails = CusOrderBLL.GetOrderDetails(customer_NIC, orderType);
+                    dataGridViewMyOrders.DataSource = OrderDetails;
+                    dataGridViewMyOrders.AutoGenerateColumns = false;
+                    dataGridViewMyOrders.DefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Regular);
+                    dataGridViewMyOrders.ColumnHeadersDefaultCellStyle.Font = new Font(Font.FontFamily, Font.Size, FontStyle.Bold);
+                    this.dataGridViewMyOrders.Columns["tbl_user"].Visible = false;
+                    this.dataGridViewMyOrders.Columns["OrderType"].Visible = false;
+                }
+            }
+            else
+            {
+                DisplayDateonLoad();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBoxOrderType.SelectedIndex = -1;
+            DisplayDateonLoad();
         }
     }
 }
